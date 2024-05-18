149096d3afc78297cde824a8097e1a39b3f4df7a
Syarif784 <144253104+syarifgithub784@users.noreply.github.com>
2024-05-18 09:15:11 +0700

    update dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,11 +27,19 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+                
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 p">
+                        <li class="nav-item">
+                          <a class="nav-link" aria-current="page" href="#">Barang</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">Member</a>
+                        </li>
+                      </ul>
                     <ul class="navbar-nav me-auto">
-
                     </ul>
 
                     <!-- Right Side Of Navbar -->
